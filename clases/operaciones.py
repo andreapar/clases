5e2c8a048bed4927b07ee561e01273628a84c8e9
andreapar <andreapar34@gmail.com>
2021-02-11 18:17:22 -0500

    clase operaciones

--- clases/operaciones.py
+++ clases/operaciones.py
@@ -1,17 +1,24 @@
+#Cómo utilizar los signos para hacer operaciones
+
+#Para sumar es super easy peasy solo debes colocar el signo
 numeroA = 57
 numeroB = 43
 sumar = numeroA + numeroB
 print(f"la suma dio {sumar} exitosamente")
 
+#Para restar también easy peasy se debe colocar el signo
 restar = numeroA - numeroB
 print(f"la resta dio {restar} exitosamente")
 
+#En la multiplicación es con el asterisco
 multiplicar = numeroA * numeroB
 print(f"la multiplicar dio {multiplicar} exitosamente")
 
+#En la divisón es con esa barrita que tu sabes
 dividir = numeroA / numeroB
 print(f"la divicion dio {dividir} exitosamente")
 
+#Para un exponente se coloca los asteriscos las veces que seas neceseario
 exponente = numeroA**numeroB
 print(f"la exponente dio {exponente} exitosamente")
 
