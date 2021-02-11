c92350c0f74704b20b38bae0c52d8849afd95e04
andreapar <andreapar34@gmail.com>
2021-02-11 18:09:54 -0500

    check

--- clases/clases2.py
+++ clases/clases2.py
@@ -1,4 +1,5 @@
-pruebaV = True
+#En esta clase aprenderemos sobre las variables booleana
+PruebaV = True
 pruebaF = False
 print( pruebaV )
 print( pruebaF )
@@ -7,6 +8,7 @@ print( pruebaV )
 
 print("-"*16, "Edad", "-"*16)
 
+#Utilizamos el mayor igual para preguntar
 edad = 18
 estatura = 1.65
 peso =  55
@@ -14,14 +16,17 @@ nombre = "Andrea Parra"
 isMayorEdad = edad >= 18
 print(isMayorEdad)
 
+#Utilizamos el signo menor
 print("-"*16, "Estatura promedio", "-"*16)
 isMayorEstatura = estatura < 1.70
 print(isMayorEstatura)
 
+#Acá se utiliza el signo diferente
 print("-"*16, "Peso diferente", "-"*16)
 isPesoDiferente = peso != 55
 print(isPesoDiferente)
 
+#Para preguntar si hay un string en un texto se copia "in"
 print("-"*16, "Esta apellido en el nombre", "-"*16)
 apellido = "Parra"
 isApellido = apellido in nombre
