2fa35e8a2456fcf29055797ab56c8c482cca7629
andreapar <andreapar34@gmail.com>
2021-02-11 18:04:16 -0500

    Check

--- clases/clases1.py
+++ clases/clases1.py
@@ -1,8 +1,10 @@
-#print ("Hola, mundo")
+#Acá empezaremos con python
+print ("Hola, mundo")
 
 saludo = "Hola, mundo"
 print (saludo)
 
+#Aprendimos acá para que es el print y cómo se utliza
 edad = 18
 estatura = 1.65
 print (saludo, edad, estatura)
