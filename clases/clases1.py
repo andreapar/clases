3f406d4babc2d39a47689a9886ac197d56cc939c
andreapar <andreapar34@gmail.com>
2021-02-11 21:21:03 -0500

    clase lista

--- clases/clases1.py
+++ clases/clases1.py
@@ -5,6 +5,8 @@ saludo = "Hola, mundo"
 print (saludo)
 
 #Aprendimos acá para que es el print y cómo se utliza
+#También algunas variables
+#Para subir es con "push"
 edad = 18
 estatura = 1.65
 print (saludo, edad, estatura)
