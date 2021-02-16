52dec41209d882f2c9b1ed33f6201abb729dbfd6
andreapar <andreapar34@gmail.com>
2021-02-16 07:04:53 -0500

    inputs

---
+++ Sesiones/inputs.py
@@ -0,0 +1,13 @@
+#----Constantes----#
+PREGUNTA_NOMBRE = "¿Cómo te llamas? : "
+PREGUNTA_EDAD = "¿Cuántos años tienes? : "
+PREGUNTA_ESTATURA ="¿Cuánto mides? : "
+MENSAJE_SALUDO = "un gusto conocerte"
+
+#----Entrada al código----#
+nombre = input(PREGUNTA_NOMBRE)
+print(MENSAJE_SALUDO, nombre)
+edad = int(input(PREGUNTA_EDAD))
+estatura = float(input(PREGUNTA_ESTATURA))
+print(edad)
+
