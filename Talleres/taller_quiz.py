4a76e81b0f573261dc2872aa3cd90d7ee7208c70
andreapar <andreapar34@gmail.com>
2021-03-11 09:28:01 -0500

    taller prequiz

--- Talleres/taller_quiz.py
+++ Talleres/taller_quiz.py
@@ -1,36 +1,38 @@
+from os import system, name
+
 # ----- Entradas -----#
 MensajeHola = "Hola, bienvenido"
-PreguntaOpcion = ''' Ingrese alguna de estas opciones
-    1. Hacer conversión de pesos a dolares o Euros
+PreguntaOpcion = ''' Menú de opciones
+    1. Hacer conversión de pesos a dolares o euros
     2. Agrege un valor a la lista de pesos
     3. Mostrar valor más alto, más bajo y promedio
     4. Eliminar elemento de la lista
     5. Salir
-:'''
+Ingrese la opción seleccionada: '''
 
-PreguntaMoneda = ''' Ingrese alguna de estas opciones
-    C. Mostar en pesos colombianos
-    D. Mostar en Dolares
-    E. Mostar en Euros
-:'''
+PreguntaMoneda = ''' Menú de conversiones
+    C. Mostrar en pesos colombianos
+    D. Mostar en dolares
+    E. Mostrar en euros
+Ingrese la opción seleccionada: '''
 
 PreguntaNumero = "Ingrese un valor en COP: "
-preguntaBorrarCoordenada = "Ingrese la posición que desea borrar:"
+PreguntaBorrarCoordenada = "Ingrese la posición que desea borrar: "
 #----- Mensajes -----#
-mensajePesos = "Mostrando lista original"
-mensajeDolares = "Mostrar lista dolares"
-mensajeEruros = "Mostar lista en euros"
-mensajeError = "Valor ingresado no válido"
-mensajeMayor = "El mayor número ingresado es: "
-mensajeMenor = "El menor número ingresado es: "
-mensajePromedio = "El promedio es: "
+MensajePesos = "Mostrando lista original"
+MensajeDolares = "Mostrar lista dolares"
+MensajeEruros = "Mostrar lista en euros"
+MensajeError = "Valor ingresado no válido"
+MensajeMayor = "El mayor número ingresado es: "
+MensajeMenor = "El menor número ingresado es: "
+MensajePromedio = "El promedio es: "
 
 #-----Conversión punto 1 -----#
 listaPesos = [2000,3000,4000,2500,1000,7600]
-listaEuros = []
+ListaEuros = []
 for elemento in listaPesos:
     convesor = round (elemento * 0.00023,2)
-    listaEuros.append(convesor)
+    ListaEuros.append(convesor)
 
 ListaDolares =[]
 for elemento in listaPesos:
@@ -39,43 +41,53 @@ for elemento in listaPesos:
 
 #----- Código -----# 
 print(MensajeHola)
-opcionEscogida = int(input(PreguntaOpcion))
-while(opcionEscogida != 5):
+OpcionEscogida = int(input(PreguntaOpcion))
+while(OpcionEscogida != 5):
+    print("")
     # ------ Opción 1 ------ #
-    if (opcionEscogida == 1):
+    if (OpcionEscogida == 1):
         opcionMoneda = input(PreguntaMoneda)
-        if (opcionMoneda == "C"):
-            print(mensajePesos)
+        if (opcionMoneda.upper() == "C"):
+            print(MensajePesos)
             print(listaPesos)
-        elif (opcionMoneda == "D"):
-            print(mensajeDolares)
+        elif (opcionMoneda.upper() == "D"):
+            print(MensajeDolares)
             print(ListaDolares)
-        elif (opcionMoneda == "E"):
-            print(mensajeEruros)
-            print(listaEuros)
+        elif (opcionMoneda.upper() == "E"):
+            print(MensajeEruros)
+            print(ListaEuros)
         else:
-            print(mensajeError)
+            print(MensajeError)
 
     #----- Opción 2 -----#
-    elif (opcionEscogida == 2):
+    elif (OpcionEscogida == 2):
         valorIngresado = float(input(PreguntaNumero))
         listaPesos.append(valorIngresado)
         print(listaPesos)
 
     #----- Opción 3 -----#
-    elif (opcionEscogida == 3):
-        print(mensajeMayor, max(listaPesos))
-        print(mensajeMenor, min(listaPesos))
-        print(mensajePromedio, sum(listaPesos)/ len(listaPesos))
-    
+    elif (OpcionEscogida == 3):
+        print(MensajeMayor, max(listaPesos))
+        print(MensajeMenor, min(listaPesos))
+        print(MensajePromedio, sum(listaPesos)/ len(listaPesos))
+
     # ----- Opción 4 -----#
-    elif (opcionEscogida == 4):
+    elif (OpcionEscogida == 4):
         print(listaPesos)
-        posicion = int(input(preguntaBorrarCoordenada)) - 1
-        listaPesos.pop(posicion)
-        print(listaPesos)
+        posicion = int(input(PreguntaBorrarCoordenada)) - 1
+        if (len(listaPesos) >= posicion):
+            listaPesos.pop(posicion)
+            print(listaPesos)
     else:
-        print(mensajeError)
-    opcionEscogida = int(input(PreguntaNumero))
+        print(MensajeError)
+
+    input("\nPresione una tecla para continuar.")
+
+    if (name == "nt"):
+        _ = system("cls")
+    else:
+        _ = system("clear")
+
+    OpcionEscogida = int(input(PreguntaOpcion))
 
 print("Feliz día")
