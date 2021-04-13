430db05ae3e7dcc710c0aa1dc843a0d29adb51cc
andreapar <andreapar34@gmail.com>
2021-04-13 10:10:36 -0500

    Introducción clases

--- Sesiones/ClasesyObjetos/introduccion.py
+++ Sesiones/ClasesyObjetos/introduccion.py
@@ -13,23 +13,22 @@ class Humano ():
         *mostrarAtributos()
             muestra los atributos del usuario
     '''
-    def __init__(self,nombreEntrada,edadEntrada):
+    def __init__(self,nombreEntrada,edadEntrada,estaturaEntrada):
         print("Hola, soy un humano nuevo")
         self.edad = edadEntrada
         self.raza = "Humano"
         self.nombre = nombreEntrada
-        self.estatura = edadEntrada
+        self.estatura = estaturaEntrada
         self.dinero = 0
 
-    
     def hablar(self,mensaje):
-        print("Hola soy {self.nombre} tengo un mensaje que decir...", mensaje)
+        print(f"Hola soy {self.nombre} tengo un mensaje que decir...", mensaje)
     
     def mostrarAtributos (self):
         print(f'''Mi nombre es {self.nombre}
-                    Mi estatura es {self.altura} metros
+                    Mi estatura es {self.estatura} metros
                     Mi edad es {self.edad} años
-                    Tengo ahorrado {self,dinero}pesos
+                    Tengo ahorrado {self.dinero} pesos
         ''')
 
     def recorrerDistancia(self,distanciaMetros):
@@ -37,23 +36,27 @@ class Humano ():
             print(f"Hola soy {self.nombre} y he recorrido {i + 1} metros")
 
     def ahorraDinero(self):
-        preguntaIngresarMonto = "Ingrese S para continuar añadiendo montos y N para finalizar"
+        preguntaIngresarMonto = "Ingrese S para continuar añadiendo montos y N para finalizar: "
         preguntaMonto = "¿Cuánto vas a ingresar?: "
         ingresarMontos = input(preguntaIngresarMonto)
         while(ingresarMontos != "N"):
-            monto = 
+            monto = float(input(preguntaMonto))
+            self.dinero = self.dinero + monto
+            print(f"Soy {self.nombre} y tengo {self.dinero} pesos")
+            ingresarMontos = input(preguntaIngresarMonto)
+        return self.dinero
 
 
-humano1 = Humano("Armin",19,1.60)
-humano2 = Humano("Kousei",20,1.67)
+humano1 = Humano('Levi',27,1.67)
+humano2 = Humano('Armin',27,1.73)
 
 humano1.hablar("Esta haciendo un día lindo")
 humano2.hablar("Adiós amigo")
 print(humano1.nombre)
 print(humano2.nombre)
-print(humano2.altura)
+print(humano2.edad)
 humano1.mostrarAtributos()
-humano2.mostrarAtributos()
 humano1.recorrerDistancia(25)
-totalAhorrado = humano2.ahorrarDinero()
-print(totalAhorrado)
+humano2.mostrarAtributos()
+totalAhorrado = humano2.ahorraDinero()
+humano2.mostrarAtributos()
